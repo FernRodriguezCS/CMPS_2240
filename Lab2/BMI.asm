88a75f5d3b34d4575978ef1af2c4f13bce5ac35a
Ferny Rodriguez <FernRodriguez328@gmail.com>
2025-09-07 15:34:13 -0700

    lab 2 done

--- Lab2/BMI.asm
+++ Lab2/BMI.asm
@@ -1,26 +1,22 @@
-# INCOMPLETE change use of temporary to saved registers for weight -> dont override
-
 .data
 str1:   .asciiz "Enter weight (in unit pound): "
 str2:   .asciiz "Enter Height (in unit inch): "
 str3:   .asciiz "Your BMI is: "
+errh:   .asciiz "\nError: height has to be greater than 0.\n"
 
 .text
         .globl  main
 
 main:
     # print first prompt
-    li      $v0,    4                               #sycall to print string
+    li      $v0,    4                                           #sycall to print string
     la      $a0,    str1
     syscall
     # read and store weight
-    li      $v0,    5                               # syscall to read int
-    syscall                                         # read int is returned in $v0
-    move    $t0,    $v0                             # t0 holds pounds
-    #print to check lbs is correct
-    # li      $v0,    1
-    # move    $a0,    $t0
-    # syscall
+    li      $v0,    5                                           # syscall to read int
+    syscall                                                     # read int is returned in $v0
+    move    $t0,    $v0                                         # t0 holds pounds
+
     # print second prompt
     li      $v0,    4
     la      $a0,    str2
@@ -29,10 +25,9 @@ main:
     li      $v0,    5
     syscall
     move    $t1,    $v0
-    #print to check height is correct
-    # li      $v0,    1
-    # move    $a0,    $t1
-    # syscall
+
+    # prevent division by zero
+    beq     $t1,    $zero,  bad_height
 
     # print out put prompt
     li      $v0,    4
@@ -41,23 +36,30 @@ main:
 
     # compute BMI and output
     #BMI(US) = Weight in pounds / (Height in inches)^2 * 703
-    li      $t2,    703
-    mult    $t1,    $t1                             # square height
-    mflo    $s0                                     # move lower 32 bits into s0 (denomenator)
-    # print to check multiplication
-    # li $v0, 1
-    # move $a0, $s0
-    # syscall
-    div $t0 , $s0
-    mflo $s1
+    # calculate denomenator
+    mult    $t1,    $t1                                         # square height
+    mflo    $t4                                                 # move lower 32 bits into t4 (denomenator)
 
-    mult $s1, $t2
-    mflo $s3
-    
+    #calculate numerator
+    li      $t2,    703
+    mult    $t0,    $t2
+    mflo    $t3
+
+    # divide
+    div     $t3,    $t4
+    mflo    $t6
+
     # print to check output
-    li $v0, 1
-    move $a0, $s3
+    li      $v0,    1
+    move    $a0,    $t6
     syscall
 
-    li      $v0,    10                              # syscall to exit
+    li      $v0,    10                                          # syscall to exit
+    syscall
+
+bad_height:
+    li      $v0,    4
+    la      $a0,    errh
+    syscall
+    li      $v0,    10
     syscall
